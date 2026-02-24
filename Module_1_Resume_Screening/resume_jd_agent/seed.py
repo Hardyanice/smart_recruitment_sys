34770d1acca2c8bcbe7b60479d96c2bcac705965
Souhardya Das <dassouhardya1720@gmail.com>
2026-02-24 10:59:33 +0530

    Delete Module_1_Resume_Screening/resume_jd_agent/seed.py

--- Module_1_Resume_Screening/resume_jd_agent/seed.py
+++
@@ -1,8 +0,0 @@
-from src.agent.database import create_job
-
-create_job(
-    "Machine Learning Engineer",
-    "Looking for Python, TensorFlow, model deployment, data preprocessing experience"
-)
-
-print("Job inserted!")
